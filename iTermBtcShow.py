fa85c31ffc7766d1d59e14f49a4aacd7e0a4e94d
watts <Wangjingnic1993@Gmail.com>
2025-06-26 11:05:36 +0800

    Create iTermBtcShow.py
    通过终端5分钟获取一次BTC/USDT 报价

---
+++ iTermBtcShow.py
@@ -0,0 +1,20 @@
+import requests
+import time
+
+def okx_get_price():
+    symbol = "BTC-USDT"
+    url = "https://www.okx.com/api/v5/market/ticker" # okx
+    try:
+        response = requests.get(url, params={"instId": symbol})
+        response.raise_for_status()
+        data = response.json()
+        last_price = float(data["data"][0]["last"])
+        ts = time.strftime('%Y-%m-%d %H:%M:%S')
+        print(f"[{ts}] {symbol} ：{last_price}")
+    except Exception as e:
+        print("获取失败：", e)
+
+# 循环轮询
+while True:
+    okx_get_price()
+    time.sleep(5)
