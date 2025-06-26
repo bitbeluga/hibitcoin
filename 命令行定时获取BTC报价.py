f97619864ea536415756fcb8a7c13d3e4a1132a2
watts <Wangjingnic1993@Gmail.com>
2025-06-26 11:06:32 +0800

    Update and rename iTermBtcShow.py to 命令行定时获取BTC报价.py

--- 命令行定时获取BTC报价.py
+++ 命令行定时获取BTC报价.py
@@ -2,7 +2,7 @@ import requests
 import time
 
 def okx_get_price():
-    symbol = "BTC-USDT"
+    symbol = "BTC-USDT" # 可以是ETH-USDT SOL-USDT
     url = "https://www.okx.com/api/v5/market/ticker" # okx
     try:
         response = requests.get(url, params={"instId": symbol})
